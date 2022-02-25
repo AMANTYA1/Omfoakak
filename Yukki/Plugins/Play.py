db91dac2ead3567f7149a197a48fdc137b1920fe
Aman Vishwakarma <95975318+AMANTYA1@users.noreply.github.com>
2022-02-25 20:41:39 +0530

    Update Play.py

--- Yukki/Plugins/Play.py
+++ Yukki/Plugins/Play.py
@@ -41,7 +41,7 @@ loop = asyncio.get_event_loop()
 
 
 @app.on_message(
-    filters.command(["ulay", f"ulay@{BOT_USERNAME}"]) & filters.group
+    filters.command(["dlay", f"dlay@{BOT_USERNAME}"]) & filters.group
 )
 @checker
 @logging
