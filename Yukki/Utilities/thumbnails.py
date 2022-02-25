f276be92f1ec3ec5117c727daa22414f636ad7d8
Aman Vishwakarma <95975318+AMANTYA1@users.noreply.github.com>
2022-02-25 22:52:24 +0530

    Update thumbnails.py

--- Yukki/Utilities/thumbnails.py
+++ Yukki/Utilities/thumbnails.py
@@ -32,10 +32,10 @@ async def gen_thumb(thumbnail, title, userid, theme, ctitle):
     Image.alpha_composite(image5, image6).save(f"cache/temp{userid}.png")
     img = Image.open(f"cache/temp{userid}.png")
     draw = ImageDraw.Draw(img)
-    font = ImageFont.truetype("Utils/finalfont.ttf", 85)
-    font2 = ImageFont.truetype("Utils/finalfont.ttf", 60)
+    font = ImageFont.truetype("Utils/umm.ttf", 85)
+    font2 = ImageFont.truetype("Utils/umm.ttf", 60)
     draw.text(
-        (20, 45),
+        (20, 46),
         f"Playing on: {ctitle[:14]}...",
         fill="white",
         stroke_width=1,
